f2bdd971e991a91cd25110df9e643bb145bd3458
Igor Ovchinnikov <faceplaces@inbox.ru>
2023-07-20 12:27:30 +0300

    first commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,12 @@
-const { merge } = require("webpack-merge");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const common = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
   },
 });
