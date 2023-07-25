123c1e52c04c425c1fe19c94f9b67c520de5160a
Igor Ovchinnikov <faceplaces@inbox.ru>
2023-07-25 13:46:20 +0300

    Reworked

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,36 @@
 export default class App {
   constructor(boardsize) {
+    this.activeHole = 1;
     this.goblin = document.createElement("div");
     this.goblin.classList.add("goblin");
+    this.createGrid(boardsize);
+    document.createElement("body").appendChild(this.createBadge());
+  }
 
-    this.activeHole = 1;
-
-    this.wrapper = document.createElement("div");
-    this.wrapper.classList.add("wrapper");
-    this.container = document.createElement("div");
-    this.container.classList.add("container");
-    this.wrapper.append(this.container);
-    this.title = document.createElement("h1");
-    this.title.classList.add("title");
-    this.title.innerText = "WACK THE GOBLIN!";
-    this.container.append(this.title);
-    this.divArray = [];
-    for (let i = 0; i < Math.pow(boardsize, 2); i += 1) {
-      this.divArray[i] = document.createElement("div");
-      this.divArray[i].classList.add("hole");
-      this.divArray[i].setAttribute("id", `hole${i}`);
-      this.container.append(this.divArray[i]);
+  createGrid(boardsize) {
+    const container = document.querySelector(".container");
+    for (let index = 0; index < Math.pow(boardsize, 2); index++) {
+      const hole = document.createElement("div");
+      hole.classList.add("hole");
+      container.appendChild(hole);
     }
-    document.body.append(this.wrapper);
   }
 
-  static getHole(index) {
-    return document.getElementById(`hole${index}`);
-  }
-
-  moveGoblin(index) {
-    App.getHole(index).append(this.goblin);
+  createBadge() {
+    const img = document.createElement("img");
+    img.setAttribute("alt", "GitHub Workflow Status (with event)");
+    img.setAttribute(
+      "src",
+      "https://img.shields.io/github/actions/workflow/status/iIgor2022/ahj_element_move/web.yml"
+    );
+    return img;
   }
 
   gamePlay() {
+    const cells = document.querySelectorAll(".hole");
     setInterval(() => {
-      let newHole = this.activeHole;
-      while (newHole === this.activeHole) {
-        newHole = Math.floor(Math.random() * this.divArray.length);
-      }
-      this.activeHole = newHole;
-      this.moveGoblin(this.activeHole);
+      const cell = cells[Math.floor(Math.random() * cells.length)];
+      cell.insertAdjacentElement("afterbegin", this.goblin);
     }, 1000);
   }
 }
